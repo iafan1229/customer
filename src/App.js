fee132dfb59602be5e6da8830dfd68b4bd236be5
unknown <home0@naver.com>
2020-06-15 15:55:20 +0900

    Apply Matiral UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: { //전체 바깥 쪽, x축으로 오버 플로우가 발생하게
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080 //테이블, 무조건 1080이상 자리잡게!, 그래서 가로 스크롤바가 생긴다.
+  }
+})
 
 const customers = [
   {
   'id' : 1,
-  'image': 'http://placeimg.com/64/64/any',
+  'image': 'http://placeimg.com/64/64/any1',
   'name': '홍길동',
   'birthday' : '1990/10/3',
   'gender' : '남',
@@ -13,18 +31,18 @@ const customers = [
   },
   {
     'id' : 2,
-    'image': 'http://placeimg.com/64/64/any',
-    'name': '홍길동',
+    'image': 'http://placeimg.com/64/64/any2',
+    'name': '이순신',
     'birthday' : '960305',
     'gender' : '남',
     'job' : '프로그래머'
   },
   {
   'id' : 3,
-  'image': 'http://placeimg.com/64/64/any1',
-  'name': '홍길동',
+  'image': 'http://placeimg.com/64/64/any3',
+  'name': '김소다',
   'birthday' : '870630',
-  'gender' : '남',
+  'gender' : '여',
   'job' : '디자이너'
   }
   ]
@@ -32,16 +50,31 @@ const customers = [
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        {
-          customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+        <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {
+        customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
             );
           }) // map은 key라는 props가 필요함. //짧은 소스 코드의 작성을 위해 map 함수의 이용. // c라는 원소로 순회 되도록! //반복문을 이용해서 반복되는 소스코드를 줄일 수 있다.
         }
-      </div>
+          </TableBody>
+        </Table>
+        </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,38 +1,21 @@
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
 
 class Customer extends React.Component { //강력한 뷰 , 기능을 제공. 리액트는 계층적인 구성이 가능. 출력할 정보가 많으면, 리액트 컴포넌트의 분리
     render() { //실제로 그려지는 내용
         return ( //요소를 반환한다.
-        <div>
-           <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name} />
-           <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job} />
-        </div>
+        <TableRow>
+          <TableCell>{this.props.id}</TableCell>
+          <TableCell><img src={this.props.image} alt="profile"></img></TableCell>
+          <TableCell>{this.props.name}</TableCell>
+          <TableCell>{this.props.birthday}</TableCell>
+          <TableCell>{this.props.gender}</TableCell>
+          <TableCell>{this.props.job}</TableCell>
+        </TableRow>
         );
     }
 }
 
-
-class CustomerProfile extends React.Component { //props의 분리
-    render() {
-      return (
-        <div>
-          <img src={this.props.image} alt="profile"/>
-          <h2>{this.props.name}({this.props.id})</h2>
-        </div>
-      )
-    }
-  }
-  
-  class CustomerInfo extends React.Component {
-    render() {
-      return (
-        <div>
-          <p>{this.props.birthday}</p>
-          <p>{this.props.gender}</p>
-          <p>{this.props.job}</p>
-        </div>
-      )
-    }
-  }
 
 export default Customer;
